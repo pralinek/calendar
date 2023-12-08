b0290dba1451a098428c90a276a4f5c8802a1d4c
Tomasz Olech <tolech89@gmail.com>
2023-12-08 12:35:33 +0100

    timezone

--- calndarhook.tsx
+++ calndarhook.tsx
@@ -1,8 +1,12 @@
 import { useState, useEffect } from 'react';
 import dayjs from 'dayjs';
 import weekOfYear from 'dayjs/plugin/week';
+import timezone from 'dayjs/plugin/timezone';
+import utc from 'dayjs/plugin/utc';
 
 dayjs.extend(weekOfYear);
+dayjs.extend(timezone);
+dayjs.extend(utc);
 
 interface MonthObject {
   monthIndex: number;
@@ -19,25 +23,36 @@ interface DayObject {
 
 interface WeekObject {
   monthIndex: number;
+  weekMonthIndex: number;
   weekStartDate: string;
   weekEndDate: string;
   daysArray: DayObject[];
 }
 
 interface MonthsAndWeeksResult {
+  currentDate: string;
+  currentMonthObject: MonthObject;
+  currentWeekObject: WeekObject;
   monthsArray: MonthObject[];
   weeksArray: WeekObject[];
 }
 
-const useMonthsAndWeeks = (currentYear: number): MonthsAndWeeksResult => {
+const useMonthsAndWeeks = (currentYear: number, timezone: string): MonthsAndWeeksResult => {
+  const [currentDate, setCurrentDate] = useState<string>('');
+  const [currentMonthObject, setCurrentMonthObject] = useState<MonthObject | null>(null);
+  const [currentWeekObject, setCurrentWeekObject] = useState<WeekObject | null>(null);
   const [monthsArray, setMonthsArray] = useState<MonthObject[]>([]);
   const [weeksArray, setWeeksArray] = useState<WeekObject[]>([]);
 
   useEffect(() => {
+    // Get current date
+    const today = dayjs().tz(timezone);
+    setCurrentDate(today.format('YYYY-MM-DD'));
+
     // Generate months array
     const generateMonthsArray = () => {
       const monthsArray: MonthObject[] = Array.from({ length: 12 }, (_, index) => {
-        const monthDate = dayjs().year(currentYear).month(index).startOf('month');
+        const monthDate = dayjs().tz(timezone).year(currentYear).month(index).startOf('month');
         return {
           monthIndex: index,
           monthName: monthDate.format('MMMM'),
@@ -47,6 +62,13 @@ const useMonthsAndWeeks = (currentYear: number): MonthsAndWeeksResult => {
       });
 
       setMonthsArray(monthsArray);
+
+      // Set current month object
+      const currentMonth = today.month();
+      const currentMonthObject = monthsArray.find((month) => month.monthIndex === currentMonth);
+      if (currentMonthObject) {
+        setCurrentMonthObject(currentMonthObject);
+      }
     };
 
     // Generate weeks array with days
@@ -55,10 +77,11 @@ const useMonthsAndWeeks = (currentYear: number): MonthsAndWeeksResult => {
 
       // Loop through each month
       monthsArray.forEach((month) => {
-        const startOfWeek = dayjs(month.startDate).startOf('week');
-        const endOfWeek = dayjs(month.endDate).endOf('week');
+        const startOfWeek = dayjs(month.startDate).tz(timezone).startOf('week');
+        const endOfWeek = dayjs(month.endDate).tz(timezone).endOf('week');
 
         let currentWeekStart = startOfWeek;
+        let weekMonthIndex = 0;
 
         // Loop through each week in the month
         while (currentWeekStart.isBefore(endOfWeek) || currentWeekStart.isSame(endOfWeek, 'day')) {
@@ -76,24 +99,36 @@ const useMonthsAndWeeks = (currentYear: number): MonthsAndWeeksResult => {
 
           weeksArray.push({
             monthIndex: month.monthIndex,
+            weekMonthIndex,
             weekStartDate: weekStartDate.format('YYYY-MM-DD'),
             weekEndDate: weekEndDate.format('YYYY-MM-DD'),
             daysArray,
           });
 
           currentWeekStart = currentWeekStart.add(1, 'week');
+          weekMonthIndex++;
         }
       });
 
       setWeeksArray(weeksArray);
+
+      // Set current week object
+      const currentWeekStart = today.startOf('week');
+      const currentWeekObject = weeksArray.find(
+        (week) =>
+          week.monthIndex === today.month() && dayjs(week.weekStartDate).isSame(currentWeekStart, 'day')
+      );
+      if (currentWeekObject) {
+        setCurrentWeekObject(currentWeekObject);
+      }
     };
 
     // Call the functions to generate arrays
     generateMonthsArray();
     generateWeeksArray();
-  }, [currentYear, monthsArray]);
+  }, [currentYear, timezone]);
 
-  return { monthsArray, weeksArray };
+  return { currentDate, currentMonthObject, currentWeekObject, monthsArray, weeksArray };
 };
 
 export default useMonthsAndWeeks;

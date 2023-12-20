fedbcb96484093c80a8d2e4c6c1b1d3a708a2065
Tomasz Olech <tolech89@gmail.com>
2023-12-20 13:09:31 +0100

    main

--- morgan.ts
+++ morgan.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import next from 'next';
 import morgan from 'morgan';
+import { NextApiHandler } from 'next';
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
@@ -11,6 +12,16 @@ app.prepare().then(() => {
 
   // Use Morgan middleware
   server.use(morgan('dev'));
+
+  // NextAuth.js handler
+  const authHandler: NextApiHandler = (req, res) => {
+    // Define the request handler for NextAuth
+    // Note: The file `pages/api/auth/[...nextauth].ts` must exist
+    return require('../../../pages/api/auth/[...nextauth]').default(req, res);
+  };
+
+  // Route for NextAuth.js
+  server.use('/api/auth', authHandler);
 
   server.get('*', (req: Request, res: Response) => {
     return handle(req, res);
